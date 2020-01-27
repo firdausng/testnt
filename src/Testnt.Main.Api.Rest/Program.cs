9e1413320b4d26218ab17cd6d76d9725a4ac2e2c
Firdaus <mail4frenz@gmail.com>
2020-01-28 07:09:36 +0800

    add serilog and seed sample project

--- src/Testnt.Main.Api.Rest/Program.cs
+++ src/Testnt.Main.Api.Rest/Program.cs
@@ -4,16 +4,62 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using Testnt.Main.Application.Seed;
 
 namespace Testnt.Main.Api.Rest
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+
+            Console.Title = "Tesnt Main";
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                // uncomment to write to Azure diagnostics stream
+                //.WriteTo.File(
+                //    @"D:\home\LogFiles\Application\identityserver.txt",
+                //    fileSizeLimitBytes: 1_000_000,
+                //    rollOnFileSizeLimit: true,
+                //    shared: true,
+                //    flushToDiskInterval: TimeSpan.FromSeconds(1))
+                .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}", theme: AnsiConsoleTheme.Literate)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting host...");
+                var host = CreateHostBuilder(args).Build();
+
+                using (var serviceScope = host.Services.GetService<IServiceScopeFactory>().CreateScope())
+                {
+                    Data.EnsureSeedData(serviceScope);
+                }
+
+                host.Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ src/Testnt.Main.Application/Seed/Data.cs
@@ -0,0 +1,58 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Testnt.Main.Application.TestProjects.Item.Command.CreateTestProjectItem;
+using Testnt.Main.Application.TestProjects.List.Query.GetTestProjectList;
+using Testnt.Main.Domain.Entity;
+using Testnt.Main.Infrastructure.Data;
+
+namespace Testnt.Main.Application.Seed
+{
+    public class Data
+    {
+        public static void EnsureSeedData(IServiceScope scope)
+        {
+            //var context = scope.ServiceProvider.GetService<TestntDbContext>();
+            //context.Database.Migrate();
+
+            //var sampleProject = context.Projects.Where(t => t.Name.Equals("Testnt")).FirstOrDefaultAsync().Result;
+            //if (sampleProject == null)
+            //{
+            //    sampleProject = new TestProject
+            //    {
+            //        Name = "Testnt",
+
+            //    };
+            //    context.Projects.Add(sampleProject);
+            //    context.SaveChanges();
+            //}
+
+            var mediator = scope.ServiceProvider.GetService<IMediator>();
+            var getTestProjectList = mediator.Send(new GetTestProjectListQuery()).Result;
+            if (getTestProjectList.Count == 0)
+            {
+                var CreateTestProjectItem = mediator.Send(new CreateTestProjectItemCommand
+                {
+                    Name = "Testnt"
+                }).Result;
+
+                if (CreateTestProjectItem.Id != null)
+                {
+                    Console.WriteLine($"{CreateTestProjectItem.Id} successfully created");
+                }
+                else
+                {
+                    Console.WriteLine($"{CreateTestProjectItem.Id} failed to be created");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Sample test project already created");
+            }
+        }
+    }
+}

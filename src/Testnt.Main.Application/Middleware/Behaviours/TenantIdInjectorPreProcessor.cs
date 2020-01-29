bb60c1d7322768e870b29e3af13eae336b8c8c5e
Firdaus <mail4frenz@gmail.com>
2020-01-30 04:06:17 +0800

    add logging

--- src/Testnt.Main.Application/Middleware/Behaviours/TenantIdInjectorPreProcessor.cs
+++ src/Testnt.Main.Application/Middleware/Behaviours/TenantIdInjectorPreProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
 using System.Threading;
 using System.Threading.Tasks;
 using Testnt.Common.Interface;
@@ -9,13 +10,17 @@ namespace Testnt.Main.Application.Middleware.Behaviours
     public class TenantIdInjectorPreProcessor<TRequest> : IRequestPreProcessor<TRequest> where TRequest : BaseRequest
     {
         private readonly ICurrentUserService currentUserService;
+        private readonly ILogger<TenantIdInjectorPreProcessor<TRequest>> logger;
 
-        public TenantIdInjectorPreProcessor(ICurrentUserService currentUserService)
+        public TenantIdInjectorPreProcessor(ICurrentUserService currentUserService, ILogger<TenantIdInjectorPreProcessor<TRequest>> logger)
         {
             this.currentUserService = currentUserService;
+            this.logger = logger;
         }
         public Task Process(TRequest request, CancellationToken cancellationToken)
         {
+            logger.LogInformation("Injecting tenant id {@TenantId} to request {@Request} for user {@User}",
+                currentUserService.TenantId, request, currentUserService.Name);
             request.TenantId = currentUserService.TenantId;
             return Task.CompletedTask;
         }

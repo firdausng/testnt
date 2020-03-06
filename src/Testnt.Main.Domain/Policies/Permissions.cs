0b2b4ce97f89db945542f2503c57bbc1f03792df
firdausng <mail4frenz@gmail.com>
2020-03-06 23:25:24 +0800

    update permissions

--- src/Testnt.Main.Domain/Policies/Permissions.cs
+++ src/Testnt.Main.Domain/Policies/Permissions.cs
@@ -11,9 +11,9 @@ namespace Testnt.Main.Domain.Policies
         ProjectRead = 0x10,
         [Display(GroupName = "Project", Name = "Create", Description = "Can create a Project entry")]
         ProjectCreate = 0x11,
-        [Display(GroupName = "Project", Name = "Update", Description = "Can update a Project entry")]
+        [Display(GroupName = "Project", Name = "Update", Description = "Can update a Project")]
         ProjectUpdate = 0x12,
-        [Display(GroupName = "Project", Name = "Delete", Description = "Can delete a Project entry")]
+        [Display(GroupName = "Project", Name = "Delete", Description = "Can delete a Project")]
         ProjectDelete = 0x13,
 
         [Display(GroupName = "Project", Name = "Alter project", Description = "Can do anything on a project")]
@@ -24,12 +24,12 @@ namespace Testnt.Main.Domain.Policies
         FeatureRead = 0x20,
         [Display(GroupName = "Feature", Name = "Create", Description = "Can create a Feature entry")]
         FeatureCreate = 0x21,
-        [Display(GroupName = "Feature", Name = "Update", Description = "Can update a Feature entry")]
+        [Display(GroupName = "Feature", Name = "Update", Description = "Can update a Feature")]
         FeatureUpdate = 0x22,
-        [Display(GroupName = "Feature", Name = "Delete", Description = "Can delete a Feature entry")]
+        [Display(GroupName = "Feature", Name = "Delete", Description = "Can delete a Feature")]
         FeatureDelete = 0x23,
 
         [Display(GroupName = "Feature", Name = "Alter Feature", Description = "Can do anything on a Feature")]
-        FeatureAdmin = 0x14,
+        FeatureAdmin = 0x24,
     }
 }
